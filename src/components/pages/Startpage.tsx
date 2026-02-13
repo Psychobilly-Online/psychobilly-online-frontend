ce072593b7817eb5834237f09d1eeaee5bb62de1
madman <madman@psychobilly.org>
2026-02-13 12:13:07 +0100

    final fixes

--- src/components/pages/Startpage.tsx
+++ src/components/pages/Startpage.tsx
@@ -7,29 +7,29 @@ export function Startpage() {
         <div className={styles.col1} id="col1">
           <div className={styles.bigBox}>
             <div className={styles.bigBoxContent}>
-            <h1>Psychobilly Online: Farewell and welcome back!</h1>
-            After the server got another PHP update, the forum software finally reached its end of
-            life and could not be updated anymore. There was no other choice but to switch it off
-            for good.
-            <br />
-            <br />
-            While it seemed as this would be the last nail to the coffin of Psychobilly Online, it
-            simply didn&apos;t want to die. A brandnew software has been installed, and all user
-            accounts have been migrated to the new forum. The only downside is that the encrypted
-            passwords could not be transferred to the new forum, so you will need to set up a new
-            password (which requires you to remember your email address you signed up with).
-            <br />
-            <br />
-            Over the next weeks and months I will work on migrating the old forums, topics and posts
-            to the new forum. This will most likely take quite some time, as the differences in the
-            data structure are complex, and as we talk about more than 1.5 million posts.
-            <br />
-            <br />
-            Anyway,{' '}
-            <a href={`${process.env.NEXT_PUBLIC_LEGACY_URL}/community`}>
-              here&apos;s the new board
-            </a>
-            !
+              <h1>Psychobilly Online: Farewell and welcome back!</h1>
+              After the server got another PHP update, the forum software finally reached its end of
+              life and could not be updated anymore. There was no other choice but to switch it off
+              for good.
+              <br />
+              <br />
+              While it seemed as this would be the last nail to the coffin of Psychobilly Online, it
+              simply didn&apos;t want to die. A brandnew software has been installed, and all user
+              accounts have been migrated to the new forum. The only downside is that the encrypted
+              passwords could not be transferred to the new forum, so you will need to set up a new
+              password (which requires you to remember your email address you signed up with).
+              <br />
+              <br />
+              Over the next weeks and months I will work on migrating the old forums, topics and
+              posts to the new forum. This will most likely take quite some time, as the differences
+              in the data structure are complex, and as we talk about more than 1.5 million posts.
+              <br />
+              <br />
+              Anyway,{' '}
+              <a href={`${process.env.NEXT_PUBLIC_LEGACY_URL}/community`}>
+                here&apos;s the new board
+              </a>
+              !
             </div>
           </div>
         </div>
